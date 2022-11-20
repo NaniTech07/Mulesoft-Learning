569684d63bfe37c64af2f1a7ceab4e020cd2a397
Nani Vasireddy <vasireddylakshman@gmail.com>
2022-11-20 12:48:47 +0530

    Adding files to git

---
+++ hello/src/main/java/hello/Nani.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Nani {
+     public static String Naniii(String s) {
+		return "Welcome "+s;
+	}
+}
---
+++ hello/src/main/java/hello/callme.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class callme {
+public String welcome(String s) {
+	return "Welcome "+s;
+}
+}
